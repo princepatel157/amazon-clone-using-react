439e228c8dc2e651eaa423a51013ef4470b0384a
Prince Patel <princepatel1576@gmail.com>
2021-01-11 22:50:37 +0530

    -

---
+++ src/Subtotal.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+
+const Subtotal = () => {
+  return <div className="subtotal"></div>;
+};
+
+export default Subtotal;

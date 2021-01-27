3ba4c8e20bcd51b4eb4e329197bb4dbc6e7ec023
Prince Patel <princepatel1576@gmail.com>
2021-01-27 23:05:03 +0530

    remove from basket function

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct(props) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  // remove item from basket
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: props.id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct_image" src={props.image} />
@@ -13,7 +24,9 @@ function CheckoutProduct(props) {
           <strong>{props.price}</strong>
         </p>
         <div className="checkoutProduct_rating">{props.rating}</div>
-        <button className="checkoutProduct_button">Remove from Busket</button>
+        <button onClick={removeFromBasket} className="checkoutProduct_button">
+          Remove from Busket
+        </button>
       </div>
     </div>
   );
